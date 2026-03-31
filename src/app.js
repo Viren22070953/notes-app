1b4d7ad586fe3b7d6a93117d07988ec1c0afe958
Viren22070953 <your-email@example.com>
2026-03-31 14:23:29 +0530

    server run

--- src/app.js
+++ src/app.js
@@ -2,10 +2,4 @@ const express=require('express');
 
 const app=express();
 
-app.listen(3000,(req,res)=>{
-    console.log('Server is running on port 3000');
-})
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
+module.exports=app;
